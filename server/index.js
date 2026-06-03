d4dad6285e9af50c3eba057cdf1e8c15938a506e
Rayan-Ahmed-developer <rayanahmeda55@gmail.com>
2026-06-03 17:34:40 +0500

    fix railway deployement link

--- server/index.js
+++ server/index.js
@@ -6,19 +6,15 @@ require('dotenv').config();
 
 let app = express();
 
-app.use(cors({
-    origin: 'https://user-inquiry.vercel.app', 
-    credentials: true
-}));
+app.use(cors()); 
 
 app.use(express.json());
 
-// Routes
 app.use('/api/website/enquiry', enquiryRouter);
 
 const PORT = process.env.PORT || 3000;
 
-// 2. Database Connection and Server Listen Sequence Fix
+
 mongoose.connect(process.env.DBURL)
     .then(() => {
         console.log("connected to MongoDB");
@@ -28,7 +24,7 @@ mongoose.connect(process.env.DBURL)
     })
     .catch((err) => {
         console.log("error connecting to MongoDB", err);
-        // DB fail bhi ho jaye to server crash na ho, taake 502 error na aaye
+   
         app.listen(PORT, () => {
             console.log("Server running on port (DB Connection Failed)", PORT);
         });
